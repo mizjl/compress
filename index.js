79b8d80823713027260c627331aed21dac320223
mizjl <121070490+mizjl@users.noreply.github.com>
2023-12-29 16:20:11 +0800

    feat:加入主要压缩代码

---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const path = require('path');
+
+const inputPath = process.argv[2];
+const compressedFile = inputPath + '.zip';
+
+const compress = (source, destination) => {
+  const sourcePath = path.join(__dirname, source);
+  const destinationPath = path.join(__dirname, destination);
+
+  // 检查源文件/目录是否存在
+  if (!fs.existsSync(sourcePath)) {
+    console.error(`错误: ${source} 不存在`);
+    return;
+  }
+
+  const output = fs.createWriteStream(destinationPath);
+  const archive = require('archiver')('zip', {
+    zlib: { level: zlib.constants.Z_BEST_COMPRESSION }
+  });
+
+  output.on('close', () => {
+    console.log(`压缩完成: ${destination}`);
+  });
+
+  archive.on('error', err => {
+    throw err;
+  });
+
+  // 如果是文件，则压缩文件
+  if (fs.statSync(sourcePath).isFile()) {
+    archive.file(sourcePath, { name: path.basename(sourcePath) });
+  } else { // 如果是目录，则压缩目录内容
+    archive.glob('**', {
+      cwd: sourcePath,
+      ignore: ['node_modules/**'],
+      dot: true,
+      onlyFiles: false
+    });
+  }
+
+  archive.pipe(output);
+
+  archive.finalize();
+};
+
+compress(inputPath, compressedFile);
